b1a11c0a19331654b936778cfd8bfe4c0c9f924d
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-04-26 23:26:46 +0300

    решение задачи 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,6 +7,21 @@ const ingredients = [
   'Приправы',
 ];
 
-Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
-    после чего вставит все li за одну операцию в список ul.ingredients.
-Для создания DOM - узлов используй document.createElement().
+// Напиши скрипт, который для каждого элемента массива ingredients создаст отдельный li,
+//     после чего вставит все li за одну операцию в список ul.ingredients.
+// Для создания DOM - узлов используй document.createElement().
+
+const ingredientsListRef = document.querySelector('#ingredients');
+console.log(ingredientsListRef);
+
+const createListIngredients = ingredients => {
+  return ingredients.map(ingredient => {
+    const itemRef = document.createElement('li');
+    itemRef.textContent = ingredient;
+    
+    return itemRef;
+  });
+};
+
+const elements = createListIngredients(ingredients);
+ingredientsListRef.append(...elements);
