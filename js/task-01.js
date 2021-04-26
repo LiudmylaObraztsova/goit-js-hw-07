803e329a8fc9d47a4381eb08f4a3abf7d8ee615c
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-04-26 22:04:51 +0300

    Исправляет разметку

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,8 @@
 const listRef = document.querySelector('#categories');
 console.log(listRef);
 
-const listItemsRef = listRef.querySelector('li.item');
+// const listItemsRef = listRef.querySelector('li.item');
+const listItemsRef = listRef.length;
 console.log(listItemsRef);
 
 console.log(`В списке ${listItemsRef.length} категории.`);
