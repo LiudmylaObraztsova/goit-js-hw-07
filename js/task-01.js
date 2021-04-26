08f7169aa555eb736d28360ca171355932706cae
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-04-26 22:13:29 +0300

    решение задачи 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,16 @@
 const listRef = document.querySelector('#categories');
-console.log(listRef);
-
-// const listItemsRef = listRef.querySelector('li.item');
-const listItemsRef = listRef.length;
-console.log(listItemsRef);
-
+// console.log(listRef);
+const listItemsRef = listRef.querySelectorAll('li.item');
+// console.log(listItemsRef);
 console.log(`В списке ${listItemsRef.length} категории.`);
+
+const countItemsRef = (items) => {
+    items.forEach(item => {
+        const titleRef = item.querySelector('h2');
+        console.log(`Категория: ${titleRef.textContent}`);
+        const itemRef = item.querySelectorAll('li');
+        console.log(`Количество элементов: ${itemRef.length}`);
+    })
+};
+
+countItemsRef(listItemsRef);
