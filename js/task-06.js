44f4b147a17eb8c311f0d3151e553e79dbb6b564
LiudmylaObraztsova <lpfoneone@gmail.com>
2021-05-02 23:08:28 +0300

    решение задачи 6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputRef = document.querySelector('#validation-input');
+const dataValue = Number(inputRef.dataset.length);
+
+const checkValue = (event) => {
+    if (dataValue === event.currentTarget.value.length) {
+        inputRef.classList.remove('invalid');
+        inputRef.classList.add('valid');
+        return;
+    }
+    inputRef.classList.remove('valid');
+    inputRef.classList.add('invalid');
+    return;
+};
+
+inputRef.addEventListener('blur', checkValue);
